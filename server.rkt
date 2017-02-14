bca2435741f75b5951f359fbc3d1236f08c2dc47
Francisco Jurado <fjurado@cimat.mx>
2017-02-14 00:02:29 -0700

    Working on server.rkt

--- server.rkt
+++ server.rkt
@@ -20,4 +20,80 @@
     (values
      create ip? ip-id ip-iw ip-body ip-waypoints ip-player)))
 
+(define (bon-appetit)
+  (universe JOIN0
+            (on-new connect)
+            (on-tick tick-tock TICK)
+            (on-msg handle-goto-message)
+            (on-disconnect disconnect)))
+
+(define (connect s iw)
+  (cond [(join? s) (add-player s iw)]
+        [(play? s) (add-spectator s iw)]))
+
+(define (disconnect s iw)
+  (cond [(join? s) (drop-client s iw)]
+        [(play? s) (drop-player s iw)]))
+
+(define (tick-tock s)
+  (cond [(join? s) (wait-or-play s)]
+        [(play? s) (move-and-eat s)]))
+
+(define (handle-goto-message s iw msg)
+  (cond [(and (play? s) (goto? msg)) (goto s iw msg)]
+        [else                        (empty-bundle s)]))
+
+(define (empty-bundle s)
+  (make-bundle s empty empty))
+
+(define (make-conection adder)
+  (lambda (u iw)
+    (define player (named-player iw))
+    (define mails (list (make-mail iw (ip-id player))))
+    (make-bundle (adder u player) mails empty)))
+
+(define (join-add-player j new-p)
+  (join (cons new-p (join-clients j)) (join-time j)))
+
+(define add-player (make-connection join-add-player))
+
+(define (named-player iw)
+  (create-player iw (symbol->string (gensym (iworld-name iw)))))
+
+(define (create-player iw n)
+  (ip iw (create-a-body PLAYER-SIZE) empty))
+
+(define (drop-client j iw)
+  (empty-bundle (join-remove j iw)))
+
+(define (join-remove j iw)
+  (join (rip iw (join-cients j)) (join-time j)))
+
+(define (rip iw players)
+  (remove iw players (lambda (iw p) (iworld=? iw (ip-iw p)))))
+
+(define (wait-or-play j)
+  (cond [(keep-waiting? j) (keep-waiting j)]
+        [else              (start-game j)]))
+
+(define (keep-waiting? j)
+  (or (> PLAYER-LIMIT (length (join-clients j)))
+      (> WAIT-TIME (join-time j))))
+
+(define (keep-waiting j)
+  (set-join-time! j (+ (join-time j) 1))
+  (time-broadcast j))
+
+(define (time-broadcast j)
+  (define iworlds (map ip-iw (join-clients j)))
+  (define load% (min 1 (/ (join-time j) WAIT-TIME)))
+  (make-bundle j (broadcast iworlds load%) empty))
+
+(define (broadcast iws msg)
+  (map (lambda (iw) (make-mail iw msg)) iws))
+
+(define (start-game j)
+  (define clients (join-clients j))
+  (define cupcakes (bake-cupcakes (length clients)))
+  (broadcast-universe (play clients cupcakes empty)))
 
